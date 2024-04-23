6db38679d50472f5d6c7e706655482e6624a5880
Rohit Kumar <107547744+thisisrohitkumar@users.noreply.github.com>
2024-04-23 10:29:55 +0530

    update : page loader to all pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
 const hamburger = document.querySelector('.hamburger')
+let hamburger_img = document.querySelector('.hamburger img')
+
+const toggleHamburger = () => {
+    
+}
 
 hamburger.addEventListener('click', () => {
+    
     const nav = document.querySelector('nav')
     nav.classList.toggle('nav__toggle')
-    hamburger.classList.toggle('ham__rot')
+
+    if(hamburger_img.src.endsWith('assets/images/close_hamburger.png'))
+        hamburger_img.src = 'assets/images/hamburger.png'
+    else
+        hamburger_img.src = 'assets/images/close_hamburger.png'
+    
 })
