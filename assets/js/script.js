bc9462fc257221a5e2a8b7407ffe0f98b2828c22
Rohit Kumar <107547744+thisisrohitkumar@users.noreply.github.com>
2024-04-26 10:43:08 +0530

    done with fighter page UI

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,30 @@ hamburger.addEventListener('click', () => {
         hamburger_img.src = 'assets/images/close_hamburger.png'
     
 })
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+    document.getElementById(tabName).classList.add("active");
+
+    // Remove "active" class from all tablinks
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove("active");
+    }
+
+    // Add "active" class to the clicked tablink
+    evt.currentTarget.classList.add("active");
+}
+
+function close_img_modal(){
+    document.querySelector('.img__modal').style.display = 'none';
+}
+
+function open_img_modal(){
+    document.querySelector('.img__modal').style.display = 'flex';
+}
